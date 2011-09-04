efc820ec7486c4bdd51211cbc186004184913bde
Orion 1337 <luca.barbara@live.com>
2011-09-04 05:20:55 +0200

    pyfacebook 0.1

---
+++ pyfacebook/__init__.py
@@ -0,0 +1,171 @@
+#!/usr/bin/python
+#
+# __init__.py
+# Copyright (C) orion 2011 <luca.barbara@live.com>
+# 
+# PyFbGraph is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# PyFbGraph is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import urllib2,urllib
+import simplejson
+
+
+GRAPH_URL = "https://graph.facebook.com/"
+access_token = ""
+def set_access_token(arg):
+	global access_token
+	access_token = arg
+	
+def get_access_token():
+	if access_token=="":
+		raise FbError("Invalid access token")
+	return str(access_token)
+
+def init(**args):
+	if "access_token" in args:
+		set_access_token(args["access_token"])
+	else:
+		if "id_app" in args:
+			id_app = args["id_app"]
+		else:
+			raise FbError("failed: no appID found")
+		if "permission" in args:
+			permission = args["permission"]
+		else:
+			permission = ""
+		set_access_token(AccessToken(id_app,permission))
+		
+class FbError(Exception):
+	def raise_error(self, message):
+		Exception.__init__(self, message)
+	
+class Request(object):
+				
+	def request(self,request,post=None):
+		if post:
+			post = urllib.urlencode(post)
+
+		res = urllib2.urlopen(request,post).read()
+		try:
+			data = simplejson.loads(res)
+			if type(data) == dict:
+				if data.get("error"):
+					raise FbError(data["error"]["message"]) 	
+				else:
+					return data
+			else:
+				return res
+		except ValueError:
+			return res
+
+class PutFile(Request):
+	def putFile(self,url,body):
+		request = urllib2.Request(url)
+		request.add_header('Content-type', 'multipart/form-data; boundary=%s'% ('PyFbGraph'))
+		request.add_header('Content-length', len(body))
+		request.add_data(body)		
+		return self.request(request)
+
+class PutObject(Request):	
+	def put_object(self,url,post=""):
+		request = urllib2.Request(url)
+		return self.request(request,post)
+	
+class GetObject(Request):	
+	def put_object(self,url):
+		request = urllib2.Request(url)
+		return self.request(request)	
+
+class DelObject(Request):	
+	def delObject(self,url):
+		request = urllib2.Request(url)
+		request.get_method = lambda: 'DELETE'
+		return self.request(request)
+
+class UploadPhoto(PutFile):
+	def upload_photo(self,id,photos):
+		import mimetypes
+		url = GRAPH_URL + id + "/photos?access_token=" + get_access_token()
+		
+		ret = []
+		for f in photos:
+			body = []
+			mimetype = mimetypes.guess_type(f["filename"])[0] or 'application/octet-stream'
+			
+			filehandle = open(f["filename"])
+		
+			#Photo description
+			body.append('--PyFbGraph')
+			body.append('Content-Disposition: form-data; name="message"')
+			body.append('')
+			body.append(f["message"])
+			#Photo content
+			body.append('--PyFbGraph')
+			body.append('Content-Disposition: file; name="source"; filename="%s"' % (f["filename"]))
+			body.append('Content-Type: %s' % mimetype)
+			body.append('')
+			body.append(filehandle.read())
+			body.append('')
+			filehandle.close()
+			
+			body.append('--PyFbGraph--')
+			body.append('')
+
+			ret.append(self.putFile(url,'\r\n'.join(body)))
+		
+		return ret
+
+class Album(UploadPhoto):	
+	pass	
+	
+class AccessToken(FbError):
+
+	def __init__(self,idapp,permission=None): 
+		self.access_token = ""
+		
+		if permission:
+			scope = "&scope=" + ",".join(permission)
+		self.REDIRECT_URL = "&redirect_uri=https://www.facebook.com/connect/login_success.html"
+		#OAUTH URL
+		self.OAUTH_URL = "https://www.facebook.com/dialog/oauth?client_id=" + idapp + self.REDIRECT_URL + scope + "&response_type=token"
+		
+		self.get_access_token()
+		if self.access_token=="":
+			raise self.raise_error("Access Token is not valid") 	
+			
+	def get_access_token(self):
+		try: 
+			from PySide.QtCore import QUrl
+			from PySide.QtGui import QApplication
+			from PySide.QtWebKit import QWebView 
+		except ImportError: 
+			raise self.raise_error("You need python-pyside\nDownload: http://developer.qt.nokia.com/wiki/PySide_Binaries_Linux")
+
+		
+		self.app = QApplication(sys.argv)
+		self.web = QWebView()
+		self.web.load(QUrl(self.OAUTH_URL))
+		self.web.loadFinished[bool].connect(self.loadfinished)
+		self.web.show()
+		self.app.exec_()
+
+	def loadfinished(self):
+		url = self.web.url().toString()
+		if url.find("https://www.facebook.com/connect/login_success.html#access_token=")>=0:
+			self.access_token = url[url.find("=")+1:url.find("&expires_in")]
+			self.web.close()
+			self.app.exit()
+			
+	def __str__(self):
+		return self.access_token	
---
+++ test.py
@@ -0,0 +1,11 @@
+import pyfacebook
+
+access_token = ""
+app_id= ""
+album_id = ""
+
+files = [{"filename":"./util/image.jpg","message":"Uploaded with PyFbGraph!1"}]
+permission = ["read_mailbox","user_photos","publish_stream","user_checkins","friends_checkins","publish_checkins","rsvp_event","read_stream","read_friendlists","manage_friendlists","user_groups","friends_groups","offline_access"]
+
+pyfacebook.init(access_token=access_token,app_id=app_id,permission=permission)
+print pyfacebook.Album().upload_photo(album_id,files)
