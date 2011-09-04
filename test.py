fd3081637e5b867efaeaf32635aa6efe2382d90a
Orion 1337 <luca.barbara@live.com>
2011-09-04 05:22:40 +0200

    pyfacebook 0.1

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import pyfacebook
-
-access_token = ""
-app_id= ""
-album_id = ""
+try:
+	from pyfacebook.common import *
+except ImportError: 
+	album_id = ""
+	app_id = ""
 
 files = [{"filename":"./util/image.jpg","message":"Uploaded with PyFbGraph!1"}]
 permission = ["read_mailbox","user_photos","publish_stream","user_checkins","friends_checkins","publish_checkins","rsvp_event","read_stream","read_friendlists","manage_friendlists","user_groups","friends_groups","offline_access"]
