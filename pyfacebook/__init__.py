3780703f87fec7842ac4937bb14880f5c9dd13e3
Orion 1337 <luca.barbara@live.com>
2011-09-06 06:28:04 +0200

    lots of changes after a lot of time

--- pyfacebook/__init__.py
+++ pyfacebook/__init__.py
@@ -21,7 +21,7 @@ import simplejson
 
 
 GRAPH_URL = "https://graph.facebook.com/"
-access_token = ""
+generic_access_token = ""
 client_access_token = None
 
 def diff_list(a,b):	
@@ -38,22 +38,11 @@ def get_client_access_token(client_id=None,client_secret=None):
 		request = urllib2.Request(url)
 		set_client_access_token(Request().request(request).split("=")[1])
 	return client_access_token
-	
-def set_access_token(arg):
-	global access_token
-	access_token = arg
-	
-	
-def get_access_token():
-	if access_token=="":
-		raise FbError("Invalid access token")
-	return str(access_token)
 
-
+	
 def init(**args):
 	if "access_token" in args:
-		set_access_token(args["access_token"])
-		
+		set_generic_access_token(args["access_token"])
 	else:
 		if "id_app" in args:
 			id_app = args["id_app"]
@@ -63,9 +52,44 @@ def init(**args):
 			permission = args["permission"]
 		else:
 			permission = ""
-		set_access_token(AccessToken(id_app,permission))
+		set_generic_access_token(AccessToken(id_app,permission))
 	
+def set_generic_access_token(access_token):
+	global generic_access_token
+	generic_access_token = access_token
+			
+def get_generic_access_token():
+	return str(generic_access_token)	
+
+class OBJECT_INIT(object):	
+	def __init__(self,id,specific_access_token=None):
+		self.set_specific_access_token(specific_access_token)
+		self.id=id
 		
+	def set_specific_access_token(self,access_token):
+		self.specific_access_token = access_token 
+			
+	def get_specific_access_token(self):
+		return str(self.specific_access_token)	
+	
+	def set_id(self,id):
+		self.id = id	
+		
+		
+class METHOD_INIT(object):
+	def __init__(self,id,specific_access_token=None):
+		self.set_specific_access_token(specific_access_token)
+		self.id=id
+	
+	def set_specific_access_token(self,access_token):
+		self.specific_access_token = access_token 
+			
+	def get_specific_access_token(self):
+		return str(self.specific_access_token)	
+	
+	def set_id(self,id):
+		self.id = id	
+			
 class FbError(Exception):
 	
 	def raise_error(self, message):
@@ -94,18 +118,21 @@ class Request(object):
 
 class PutFile(Request):
 	
-	def putFile(self,url,body):
+	def putFile(self,**args):
+		url = "%s%s/photos/?access_token=%s" % (GRAPH_URL,self.id,self.specific_access_token if self.specific_access_token else generic_access_token)
+		print url
+		
 		request = urllib2.Request(url)
 		request.add_header('Content-type', 'multipart/form-data; boundary=%s'% ('PyFbGraph'))
-		request.add_header('Content-length', len(body))
-		request.add_data(body)		
+		request.add_header('Content-length', len(args["body"]))
+		request.add_data(args["body"])		
 		return self.request(request)
 
 
 class PutObject(Request):	
 	
 	def put_object(self,**args):
-		url = "%s%s/%s?access_token=%s" % (GRAPH_URL, args["id"], args["comp"], args["access_token"] if "access_token"  in args else get_access_token())
+		url = "%s%s/%s?access_token=%s" % (GRAPH_URL, self.id, args["comp"], self.specific_access_token if self.specific_access_token else generic_access_token)
 		print url
 		request = urllib2.Request(url)
 		return self.request(request,args["post"])
@@ -122,25 +149,45 @@ class GetObject(Request):
 class DelObject(Request):	
 	
 	def del_object(self,**args):
-		url = "%s%s/%s?access_token=%s" % (GRAPH_URL, args["id"], args["comp"], args["access_token"] if "access_token"  in args else get_access_token())
+		url = "%s%s/%s?access_token=%s" % (GRAPH_URL, self.id, args["comp"], self.specific_access_token if self.specific_access_token else generic_access_token)
+		print url
 		request = urllib2.Request(url)
 		request.get_method = lambda: 'DELETE'
 		return self.request(request)
 
-
-class Account(PutObject,DelObject):
+class Feed(METHOD_INIT,PutObject):
+	#create a link, post or status message
+	def create_post(self,parameter):
+		return self.put_object(comp ="feed" , post =parameter)
 	
-	def create_account(self,id,client_access_token,parameter=None):
-		return self.put_object(id = id, comp ="accounts/test-users" , post =parameter,access_token=access_token)
+class Account(METHOD_INIT,PutObject,DelObject):
+	
+	def create_account(self,parameter=None):
+		return self.put_object( comp ="accounts/test-users" , post =parameter)
 		
-	def delete_account(self,id,client_access_token):
-		return self.del_object(id = id, comp ="",access_token=client_access_token)
+	def delete_account(self):
+		return self.del_object( comp ="")
 
+			
+class Comment(METHOD_INIT,PutObject):
+	def comment(self,message):
+		return self.put_object( comp ="comments" , post ={"message":message})
+	
+	
+class Likes(METHOD_INIT,PutObject,DelObject):
+	
+	def like(self):
+		return self.put_object(comp ="likes" , post ="")	
+	
+	
+	def unlike(self):
+		return self.del_object(comp ="likes")	
+	
+	
 class UploadPhoto(PutFile):
 	
-	def upload_photo(self,id,photos):
+	def upload_photo(self,photos):
 		import mimetypes
-		url = GRAPH_URL + id + "/photos?access_token=" + get_access_token()
 		
 		ret = []
 		for f in photos:
@@ -166,64 +213,35 @@ class UploadPhoto(PutFile):
 			body.append('--PyFbGraph--')
 			body.append('')
 
-			ret.append(self.putFile(url,'\r\n'.join(body)))
+			ret.append(self.putFile(body='\r\n'.join(body)))
 		
-		return ret
-
-
-			
-class Comment(PutObject):
+		return ret	
 	
-	def comment(self,id,message):
-		return self.put_object(id = id, comp ="comments" , post ={"message":message})
-	
-	
-class Likes(PutObject,DelObject):
-	
-	def like(self,id):
-		return self.put_object(id = id, comp ="likes" , post ="")	
-	
-	
-	def unlike(self,id):
-		return self.del_object(id = id, comp ="likes")	
-	
-	
-class Connections(GetObject):
+class Connection(METHOD_INIT,GetObject):
 	
 	CONN = []
 	
-	def connection(self,id,connection,**args):
+	def connection(self,connection=[],**args):
 		get = []
 		
-		if connection in self.CONN:
+		if connection in self.CONN or not self.CONN:
 			
 			for a in args.keys():
 				get.append("%s=%s" % (a,args[a]))
-			get.append("access_token=%s" % (args["access_token"] if "access_token"  in args else get_access_token()))
-			url = "%s%s/%s?%s" % (GRAPH_URL, id ,connection ,"&".join(get))
+			get.append("access_token=%s" % (self.specific_access_token if self.specific_access_token else generic_access_token))
+			url = "%s%s/%s?%s" % (GRAPH_URL, self.id ,connection ,"&".join(get))
 			return self.get_object(url)
 		else:
 			return "Unknown connection: %s " % (connection) 
 	
 	
-	def connections(self,args):
-		data = []
-		for a in args:
-			if "access_token" not in a:
-				access_token=""
-			data.append(self.connection(a["id"],a["connection"],access_token))
-		return data
-	
-	
-class Object(GetObject):
+class Object(METHOD_INIT,GetObject):
 	
 	FIELDS = []
 	
-	def object(self,id,**args):	
+	def object(self,**args):	
 		
 		get = []
-		if access_token == "":
-			access_token = get_access_token()
 			
 		if "fields" in args:
 			d = diff_list(self.FIELDS,args["fields"])
@@ -231,8 +249,8 @@ class Object(GetObject):
 				return "Unknown(s) field(s): %s" % (",".join(d))
 			get.append("fields=%s" % (",".join(args["fields"])))
 			
-		get.append("access_token=%s" % (args["access_token"] if "access_token"  in args else get_access_token()))
-		url = "%s%s/?%s" % (GRAPH_URL,id,"&".join(get))
+		get.append("access_token=%s" % (self.specific_access_token if self.specific_access_token else generic_access_token))
+		url = "%s%s/?%s" % (GRAPH_URL,self.id,"&".join(get))
 		
 		return self.get_object(url)
 
@@ -240,21 +258,21 @@ class Object(GetObject):
 
 
 """FACEBOOK GRAPH OBJECTS"""	
-class Album(UploadPhoto,Object,Connections,Comment,Likes):	
+class Album(OBJECT_INIT,UploadPhoto,Object,Connection,Comment,Likes):	
 	
 	CONN = ["photos","likes","comments","picture"]	
 	FIELDS = ["id","from","name","description","location","link","cover_photo",
 			"privacy","count","type","created_time","updated_time"]
 	
-class Application(Object,Connections,Account):	
+class Application(OBJECT_INIT,Object,Connection,Account):	
 	
 	CONN = ["accounts","albums","feed","insights","links","picture","posts",
 			"reviews","staticresources","statuses","subscriptions","tagged","translations",
 			"scores","achievements"]	
 	FIELDS = ["id","name","description","category","subcategory","link"]
 	
-	
-	
+class User(OBJECT_INIT,Object,Connection,Feed):
+	pass
 		
 class AccessToken(FbError):
 
--- test.py
+++ test.py
@@ -13,38 +13,48 @@ permission = ["read_mailbox","user_photos","publish_stream","user_checkins","fri
 #init library
 pyfacebook.init(access_token=access_token,app_id=app_id,permission=permission)
 
-#print the profile that created this album and the title of the album 
-print pyfacebook.Album().object(album_id,fields=["from","name"])
-data = pyfacebook.Album().connections([{"id":album_id,"connection":"photos"},{"id":album_id,"connection":"picture"}])
 
+print pyfacebook.User("me").create_post({"link":"http://www.google.it","message":"test"})
+print pyfacebook.User("me",access_token).create_post({"link":"http://www.google.it","message":"test"})
+print pyfacebook.Feed("me").create_post({"link":"http://www.google.it","message":"test"})
+print pyfacebook.Feed("me",access_token).create_post({"link":"http://www.google.it","message":"test"})
+
+print pyfacebook.User("me").object()
+print pyfacebook.Object("me").object()
+
+#print the profile that created this album and the title of the album 
+print pyfacebook.Album(album_id).object(fields=["from","name"])
 #The photos contained in this album
-print data[0]
+print pyfacebook.Album(album_id,access_token).connection("photos")
+
 
 #The album's cover photo
 f = open("test.jpg","w")
-f.write(data[1])
+f.write(pyfacebook.Connection(album_id,access_token).connection("picture"))
 f.close()
+
 
 #Add photos to an album
 files = [{"filename":"./util/image.jpg","message":"Uploaded with PyFbGraph!1"}]
-print pyfacebook.Album().upload_photo(album_id,files)
+print pyfacebook.Album(album_id).upload_photo(files)
+
 
 #Add comment to an album
-print pyfacebook.Album().comment(album_id,"test")
-#like an Album
-print pyfacebook.Album().like(album_id)
+ret = pyfacebook.Album(album_id).comment('test')
+print ret
+#like a comment
+print pyfacebook.Likes(ret['id']).like()
 #unlike an Album
-print pyfacebook.Album().unlike(album_id)
+print pyfacebook.Album(album_id).unlike()
 
 #The photo albums this page has created
-print pyfacebook.Application().connection("2439131959","albums")
-print pyfacebook.Application().connection("2439131959","albums",limit=25,until=1247270845)
+print pyfacebook.Application("2439131959").connection("albums")
+print pyfacebook.Application("2439131959").connection("albums",limit=25,until=1247270845)
 # =
-print pyfacebook.GetObject().get_object("https://graph.facebook.com/2439131959/albums?access_token={ACCESSTOKEN}&limit=25&until=1247270845")
+print pyfacebook.GetObject().get_object("https://graph.facebook.com/2439131959/albums?access_token=%s&limit=25&until=1247270845" % (access_token))
 
-client_access_token = pyfacebook.get_client_access_token(app_id, app_secret)
-print client_access_token
+
 #Create a test account for an application
-print pyfacebook.Application().create_account(app_id, client_access_token, parameter={"name":"lolasd","installed":True})
+print pyfacebook.Application(app_id,pyfacebook.get_client_access_token(app_id, app_secret)).create_account(parameter={"name":"lolasd","installed":True})
 #Delete a test account for an application
-print pyfacebook.Application().delete_account("id test user",client_access_token)
+print pyfacebook.Application("test user id",pyfacebook.get_client_access_token(app_id, app_secret)).delete_account()
